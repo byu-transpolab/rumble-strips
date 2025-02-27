6a5823fd8e9779ab60d4690cffb067f16c2e5043
Tyler <carrutht@byu.edu>
2025-02-27 08:35:51 -0700

    rearranged some lines in function_tester

--- R/function_tester.R
+++ R/function_tester.R
@@ -1,10 +1,9 @@
-# Load the functions from functions_list.R
-source("R/functions_list.R")
-
-
 library(tidyverse)
 library(googlesheets4)
 gs4_deauth() #prevents the need of signing into Google
+
+# Load the functions from functions_list.R
+source("R/functions_list.R")
 
 tib <- station_data(720)
 
@@ -23,3 +22,4 @@ obs_time(8, 17, n, hv)
 plot_station(tib, 
              "2023-05-01", "2023-08-31",
              8, 17)
+
