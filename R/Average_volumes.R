a8b2e7df30291c158f65b5821e3e2aee4ed5d124
Tyler <carrutht@byu.edu>
2025-02-27 09:00:55 -0700

    Created Avgerage_volumes script

---
+++ R/Average_volumes.R
@@ -0,0 +1,9 @@
+##Description#############################################
+#' A script that takes in a csv file with a list of station 
+#' numbers and averages the volume data together. 
+#' Will produce individual plots for all stations as well as 
+#' final plot showing the overall average.
+#' Creates a tibble with a summary of each stations data 
+#' to further compare each station.
+
+##packages##################################################
